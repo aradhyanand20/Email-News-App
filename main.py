4002dffd37f46924347e5446b58fd9fbe927cb14
aradhyanand20 <aradhyanand24k@gmail.com>
2025-11-09 01:25:40 +0530

    email can be send

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
 import requests
 import json
 from rich import print_json
-
+from send_email import send_email
 api_key = "8994f1a1c11840c0984ee56e040227db"
 url = "https://newsapi.org/v2/everything?q=tesla&from=" \
-"2025-10-05&sortBy=publishedAt&apiKey" \
-"=8994f1a1c11840c0984ee56e040227db"
+"2025-10-08&sortBy=publishedAt&apiKey=8994f1a1c11840c09" \
+"84ee56e040227db"
 request = requests.get(url)
 content = request.json()
-print(json.dumps(content, indent=4,sort_keys=True))
-print_json(data=content)
-print(content["articles"])
-for article in content["articles"]:
-    print(article['title'])
-    print(article['description'])
+print_json(json.dumps(content))
+body = ""
+for articles in content["articles"]:
+    if articles["title"] is not None:
+        body =  body + articles["title"] + "\n" + articles["description"] + 2*"\n"
+body = body.encode("utf-8")
+send_email(message = body)
