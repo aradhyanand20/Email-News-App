d84b323bf6ad5f3db9a1b7df218bcf64623fa9ad
aradhyanand20 <aradhyanand24k@gmail.com>
2025-11-09 22:52:16 +0530

    add subject, url, converted to english lang and categorized content

--- main.py
+++ main.py
@@ -2,16 +2,17 @@ import requests
 import json
 from rich import print_json
 from send_email import send_email
+topic="tesla"
 api_key = "8994f1a1c11840c0984ee56e040227db"
-url = "https://newsapi.org/v2/everything?q=tesla&from=" \
-"2025-10-08&sortBy=publishedAt&apiKey=8994f1a1c11840c09" \
-"84ee56e040227db"
+url = f"https://newsapi.org/v2/everything?q={topic}" \
+"&from=2025-10-09&sortBy=publishedAt&apiKey=8994f1a1c11840c0984ee56e040227db" \
+"&language=en"
 request = requests.get(url)
 content = request.json()
 print_json(json.dumps(content))
 body = ""
-for articles in content["articles"]:
+for articles in content["articles"][:20]:
     if articles["title"] is not None:
-        body =  body + articles["title"] + "\n" + articles["description"] + 2*"\n"
+        body =  "Subject: Today's News"+ "\n" + body + articles["title"] + "\n" + articles["description"] + "\n" + articles["url"] + 2*"\n"
 body = body.encode("utf-8")
 send_email(message = body)
